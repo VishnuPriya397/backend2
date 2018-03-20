bb098d5a352b575ce9b1748cc88a992521a93ef7
Priya <Priya@192.168.1.6>
2018-03-20 22:12:58 +0530

    Friend List

--- src/main/java/com/Niit/Dao/FriendDAO.java
+++ src/main/java/com/Niit/Dao/FriendDAO.java
@@ -2,10 +2,21 @@ package com.Niit.Dao;
 
 import java.util.List;
 
+import com.Niit.model.Friend;
 import com.Niit.model.User;
 
 public interface FriendDAO {
-
-	public List<User> suggestedUsers(String email);
 	
+    List<User> suggestedUsers(String email);
+	
+	void addFriend(Friend friend);
+
+	List<Friend> pendingRequests(String email);
+
+	void acceptRequest(Friend request);
+
+	void deleteRequest(Friend request);
+
+	public List<Friend> listOfFriends(String email);
+
 }
--- src/main/java/com/Niit/Dao/FriendDAOImpl.java
+++ src/main/java/com/Niit/Dao/FriendDAOImpl.java
@@ -3,26 +3,30 @@ package com.Niit.Dao;
 import java.util.List;
 
 import javax.transaction.Transactional;
+
+import org.hibernate.Query;
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.Niit.model.Friend;
 import com.Niit.model.User;
 
 @Repository
 @Transactional
 public class FriendDAOImpl implements FriendDAO {
-
+	
     public FriendDAOImpl(){
 		
 		System.out.println("FriendDAOImpl");
 	}
 	
-	@Autowired 
+	@Autowired
 	private SessionFactory sessionFactory;
-	
+
+
 	public List<User> suggestedUsers(String email) {
 		Session session=sessionFactory.openSession();
 		SQLQuery sqlQuery=session.createSQLQuery("select * from user_table where email in " 
@@ -36,8 +40,52 @@ public class FriendDAOImpl implements FriendDAO {
 		sqlQuery.setString(2, email);
 		sqlQuery.addEntity(User.class);
 		List<User> suggestedUsersList=sqlQuery.list();
-		session.close();
-		return suggestedUsersList;	
+		return suggestedUsersList;
+
+	}
+
+	public void addFriend(Friend friend) {
+         
+		Session session=sessionFactory.getCurrentSession();
+		session.save(friend);	
 		
 	}
+
+	public List<Friend> pendingRequests(String email) {
+	
+		Session session=sessionFactory.getCurrentSession();
+		Query query=session.createQuery("from Friend where status=? and toId.email=?");
+		query.setString(1, email);
+		query.setCharacter(0, 'p');
+		List<Friend> pendingRequests=query.list();
+		return pendingRequests;
+	}
+
+	public void acceptRequest(Friend request) {
+		
+		Session session=sessionFactory.getCurrentSession();
+		request.setStatus('a');
+		session.update(request);
+	}
+
+	public void deleteRequest(Friend request) {
+		
+		Session session=sessionFactory.getCurrentSession();		
+		session.delete(request);
+	}
+	
+	public List<Friend> listOfFriends(String email) {
+		Session session=sessionFactory.getCurrentSession();
+		Query query1=session.createQuery("select  f.toId from Friend f where f.fromId.email=? and f.status=?");
+		query1.setString(0, email);
+		query1.setCharacter(1, 'a');
+		List<Friend> friendsList1=query1.list();
+		Query query2=session.createQuery("select f.fromId from Friend f where f.toId.email=? and f.status=?");
+		query2.setString(0, email);
+		query2.setCharacter(1, 'a');
+		List<Friend> friendsList2=query2.list();
+		friendsList1.addAll(friendsList2);
+		return friendsList1;
+	 }
+	
 }
